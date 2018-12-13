e0087c68fc38e60a11ea9ddf368423fe54ec4970
hunnycircle <hunny@tea.nifty.jp>
2018-12-13 23:50:30 +0900

    feature-#1

---
+++ srcs/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace sghr
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string ListFileName = System.AppDomain.CurrentDomain.BaseDirectory+"tasklist.txt";
+        public static string LogFileName = System.AppDomain.CurrentDomain.BaseDirectory + "tasklog.txt";
+        public static Boolean Init = false;
+
+        public static int AddRecord( string taskname )
+        {
+            int retval = 0;
+
+            try
+            {
+                using (StreamWriter fp = new StreamWriter(App.LogFileName, true, Encoding.UTF8))
+                {
+                    fp.WriteLine(taskname + "\t" + System.DateTime.Now);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                retval = -1;
+            }
+
+            return retval ;
+        }
+
+        private void AppStartup(object sender, StartupEventArgs e)
+        {
+            foreach (string arg in e.Args)
+            {
+                switch (arg)
+                {
+                    case "/init":
+                        Init = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ srcs/ToolWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sghr
+{
+    /// <summary>
+    /// Window1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ToolWindow : Window
+    {
+        public ToolWindow()
+        {
+            if (!App.Init)
+            {
+                Left = Properties.Settings.Default.STARTUP_X;
+                Top = Properties.Settings.Default.STARTUP_Y;
+            }
+            InitializeComponent();
+        }
+
+        public void WindowClosing(object sender, CancelEventArgs e)
+        {
+            App.AddRecord(Properties.Resources.STR_LOG_END);
+
+            Properties.Settings.Default.STARTUP_X = Left;
+            Properties.Settings.Default.STARTUP_Y = Top;
+            Properties.Settings.Default.Save();
+        }
+        
+        public void GridLoaded(object sender, RoutedEventArgs e)
+        {
+            App.AddRecord(Properties.Resources.STR_LOG_START);
+
+            TaskListInit();
+            TaskSelector.SelectedIndex = 0;
+        }
+
+        public void TaskSelectorChanged(object sender, RoutedEventArgs e)
+        {
+            if( App.AddRecord(TaskSelector.SelectedItem.ToString()) != 0 ) {
+                Application.Current.Shutdown();
+            }
+        }
+
+        public void TaskListInit()
+        {
+            TaskSelector.Items.Clear();
+            TaskSelector.Items.Add(Properties.Resources.STR_REC_PAUSE);
+
+            try
+            {
+                using (StreamReader fp = new StreamReader(App.ListFileName))
+                {
+                    while (fp.Peek() >= 0)
+                    {
+                        TaskSelector.Items.Add(fp.ReadLine());
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
